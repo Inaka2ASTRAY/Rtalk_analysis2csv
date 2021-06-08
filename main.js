0a8ed7a0fe0dc02195310d910178ac73e4658518
ASTRAY <astraylmo@gmail.com>
2021-06-08 22:30:12 +0900

    add css media type and add calc pattern

--- main.js
+++ main.js
@@ -1,14 +1,25 @@
 function minutes_conv(times){
     // hms と トーク時間を分に直す
+    // 59分以上の値、59秒以上の値をあたえられたら…?erorrでalertとか
     let time_arr=times.split(":");
-    if(time_arr.length==3){
+    if(time_arr.length==3 && time_arr[0]!=""){
 	hou=parseInt(time_arr[0])*60;
-	min=parseInt(time_arr[1]);
+	min=parseInt(time_arr[1]); // 空だった時の判定をどうするか
 	mins=hou+min;
+    }else if(time_arr.length<=1 && time_arr[2]!=""){
+	mins=parseFloat(time_arr[2])/60;
     }else{
-	  min=parseInt(time_arr[0]);
-	  sec=parseInt(time_arr[1])/60;
-	  mins=min+sec;
+	if(time_arr.length==3){
+	    min=parseInt(time_arr[1]);
+	    sec=parseFloat(time_arr[2])/60;
+	    mins=min+sec;
+	}else{
+	    mins=parseInt(time_arr[0]);
+	    if(time_arr[1]){
+		sec=parseFloat(time_arr[1])/60;
+		mins=mins+sec;
+	    }
+	}
       }
     return parseFloat(mins);
       }
@@ -16,6 +27,9 @@ function lishit(ttitle,pvAll,play_Sum,pArv,pbTime){
     // 累計再生数からアプリ内再生数を抽出
     let pTime=minutes_conv(play_Sum);
     let pBack=minutes_conv(pbTime);
+    if(12<pBack){
+	pBack=12;
+    }
     let avr_pb=pArv*pBack;
     let ps_par=pTime*100;
     let pv_app=ps_par/avr_pb+0.5;
@@ -33,21 +47,23 @@ const app_pv={
 	return{
 	    title: '',
 	    pv: '',
-	    pt: '',
+	    pt_h: '',
+	    pt_m: '',
+	    pt_s: '',
 	    pa: '',
-	    pb: '',
+	    pb_m: '',
+	    pb_s: '',
 	    pv_arr: ''
 	}
     },
     methods:{
 	sent_csv(){
-    	    this.pv_arr=lishit(this.title,this.pv,this.pt,this.pa,this.pb);
+	    let totaltime=this.pt_h+":"+this.pt_m+":"+this.pt_s;
+	    let playback=this.pb_m+":"+this.pb_s;
+	    console.log("再生時間"+totaltime);
+	    console.log("トーク時間"+playback);
+    	    this.pv_arr=lishit(this.title,this.pv,totaltime,this.pa,playback);
 	    list[this.pv_arr[0]]=[this.pv_arr[1],this.pv_arr[2],this.pv_arr[3],this.pv_arr[4],this.pv_arr[5],this.pv_arr[6]];
-	    // this.title= '';
-	    // this.pv= '';
-	    // this.pt= '';
-	    // this.pa= '';
-	    // this.pb= ''; //入力した値を消すために空にする
 	},
 	dl_csv(){
 	    // listからcsvを生成
